f61d48d91b8bf8b0b984afaef945b339d5da6e1f
Neha <53373823+nehapv@users.noreply.github.com>
2020-12-03 15:08:49 +0530

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,8 @@
+
+        // function validateForm(){
+        //     var x = document.forms[""][""].value;
+        //      if (x == " ") {
+        //          alert("Name must be filled out");
+        //         return false;
+        //     }
+        // }
